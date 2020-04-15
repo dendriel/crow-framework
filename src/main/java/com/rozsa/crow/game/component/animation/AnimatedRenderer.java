008b8922c2c735076ec82f74209be0e3e8cf60f8
vitor.rozsa <vitor.rozsa@hotmail.com>
2020-04-15 07:55:16 -0300

    Add animation layers to animated renderer
    - This way, we can setup different context of animations and switch
    between these context transparently.

--- src/main/java/com/rozsa/crow/game/component/animation/AnimatedRenderer.java
+++ src/main/java/com/rozsa/crow/game/component/animation/AnimatedRenderer.java
@@ -3,21 +3,23 @@ package com.rozsa.crow.game.component.animation;
 import com.rozsa.crow.game.component.Position;
 import com.rozsa.crow.game.component.StaticRenderer;
 import com.rozsa.crow.screen.api.Drawable;
-import com.rozsa.crow.screen.attributes.Size;
 
-import java.util.ArrayList;
 import java.util.HashMap;
 import java.util.List;
 import java.util.Map;
 
 public class AnimatedRenderer<TKey> extends StaticRenderer {
     public static final String DEFAULT_ANIMATED_RENDERER = "_defaultAnimatorComponent";
-    private final Map<TKey, Animation> animations;
+    private static final Integer DEFAULT_ANIMATIONS_LAYER = 0;
+
+    private final Map<Integer, Map<TKey, Animation>> animationsLayers;
+    private Integer animationLayer;
 
     public AnimatedRenderer(Position position, int layer, String name, boolean flipX, boolean flipY) {
         super(position, layer, name, flipX, flipY);
 
-        animations = new HashMap<>();
+        animationsLayers = new HashMap<>();
+        this.animationLayer = DEFAULT_ANIMATIONS_LAYER;
     }
 
     @Override
@@ -28,12 +30,25 @@ public class AnimatedRenderer<TKey> extends StaticRenderer {
             return;
         }
 
+        Map<TKey, Animation> animations = getCurrentAnimations();
         animations.values().forEach(Animation::update);
         refreshDrawings();
     }
 
+    private Map<TKey, Animation> getCurrentAnimations() {
+        return animationsLayers.get(animationLayer);
+    }
+
+    private Map<TKey, Animation> getAnimations(int layer) {
+        if (!animationsLayers.containsKey(layer)) {
+            animationsLayers.put(layer, new HashMap<>());
+        }
+        return animationsLayers.get(layer);
+    }
+
     private void refreshDrawings() {
         drawings.clear();
+        Map<TKey, Animation> animations = getCurrentAnimations();
         for (Animation animation : animations.values()) {
             if (animation.isActive()) {
                 drawings.addAll(animation.getDrawings(this));
@@ -46,15 +61,53 @@ public class AnimatedRenderer<TKey> extends StaticRenderer {
         return drawings;
     }
 
+    public void setAnimationLayer(int layer) {
+        if (animationsLayers.containsKey(layer)) {
+            this.animationLayer = layer;
+        }
+    }
+
+    public int getAnimationLayer() {
+        return animationLayer;
+    }
+
     public void add(TKey key, Animation animation) {
+        add(key, animation, animationLayer);
+    }
+
+    public void add(TKey key, Animation animation, int layer) {
+        Map<TKey, Animation> animations = getAnimations(layer);
         animations.put(key, animation);
     }
 
     public void setActive(TKey key, boolean isActive) {
+        setActive(key, isActive, animationLayer);
+    }
+
+    public void setActive(TKey key, boolean isActive, int layer) {
+        Map<TKey, Animation> animations = getAnimations(layer);
         if (!animations.containsKey(key)) {
             return;
         }
 
         animations.get(key).setActive(isActive);
+    }
+
+    public boolean isAllAnimationsInactive() {
+        return isAllAnimationsInactive(animationLayer);
+    }
+
+    public boolean isAllAnimationsInactive(int layer) {
+        Map<TKey, Animation> animations = getAnimations(layer);
+        return animations.values().stream().noneMatch(Animation::isActive);
+    }
+
+    public void setAllAnimationsInactive() {
+        setAllAnimationsInactive(animationLayer);
+    }
+
+    public void setAllAnimationsInactive(int layer) {
+        Map<TKey, Animation> animations = getAnimations(layer);
+        animations.values().forEach(a -> a.setActive(false));
     }
 }
